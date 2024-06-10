a65accb8ca804d2c385ca4cfb38b1be51625c25f
Matěj Pařízek <parizmat@fit.cvut.cz>
2024-06-10 15:02:24 +0200

    [Semestral:] Update permission

--- app/src/main/java/cz/ctu/fit/bi/and/parizmat/semestral/feature/permisions/Permission.kt
+++ app/src/main/java/cz/ctu/fit/bi/and/parizmat/semestral/feature/permisions/Permission.kt
@@ -2,19 +2,18 @@ package cz.ctu.fit.bi.and.parizmat.semestral.feature.permisions
 
 import android.Manifest
 import android.os.Build
-import androidx.compose.foundation.background
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import cz.ctu.fit.bi.and.parizmat.semestral.R
+import cz.ctu.fit.bi.and.parizmat.semestral.core.presentation.ui.ErrorScreen
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
@@ -23,13 +22,13 @@ fun Permission(
     content: @Composable () -> Unit = {},
 ) {
     val permissions: MutableList<String> = mutableListOf()
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        permissions += Manifest.permission.POST_NOTIFICATIONS
-    }
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
         permissions += Manifest.permission.ACTIVITY_RECOGNITION
     }
-    permissions+=Manifest.permission.INTERNET
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+        ErrorScreen("Sorry, for this app is your android version is low")
+    }
+    permissions += Manifest.permission.INTERNET
 
     val permissionState = rememberMultiplePermissionsState(permissions)
     if (permissionState.allPermissionsGranted) {
